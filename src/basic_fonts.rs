bc61cd9fcd0a2902afc17920af0f51b2582fe8c9
follow39 <ingang39@gmail.com>
2022-08-15 22:57:58 +0200

    Minor fix

--- src/basic_fonts.rs
+++ src/basic_fonts.rs
@@ -5,7 +5,7 @@ pub enum BasicFonts {
     Banner,
 }
 
-pub(crate) fn get_font_data_string(basic_font: &BasicFonts) -> &str {
+pub(crate) const fn get_font_data_string(basic_font: &BasicFonts) -> &str {
     match basic_font {
         BasicFonts::Big => include_str!("../fonts/big_font.txt"),
         BasicFonts::Banner => include_str!("../fonts/banner_font.txt"),
