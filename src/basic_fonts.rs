68c850453487404aaf97d839d045c240ab5be390
follow39 <ingang39@gmail.com>
2022-08-15 17:32:26 +0200

    Remove lazy_static

--- src/basic_fonts.rs
+++ src/basic_fonts.rs
@@ -1,7 +1,3 @@
-use lazy_static::lazy_static;
-
-use std::collections::HashMap;
-
 #[derive(PartialEq, Eq, Clone, Hash)]
 pub enum BasicFonts {
     Block,
@@ -9,20 +5,14 @@ pub enum BasicFonts {
     Banner,
 }
 
-static BLOCK_FONT_FILE: &'static str = include_str!("../fonts/block_font.txt");
-static BANNER_FONT_FILE: &'static str = include_str!("../fonts/banner_font.txt");
-static BIG_FONT_FILE: &'static str = include_str!("../fonts/big_font.txt");
-
-lazy_static! {
-    static ref BASIC_FONTS_DATA: HashMap<BasicFonts, &'static str> = {
-        let mut basic_fonts_data_dict = HashMap::new();
-        basic_fonts_data_dict.insert(BasicFonts::Big, BIG_FONT_FILE);
-        basic_fonts_data_dict.insert(BasicFonts::Banner, BANNER_FONT_FILE);
-        basic_fonts_data_dict.insert(BasicFonts::Block, BLOCK_FONT_FILE);
-        basic_fonts_data_dict
-    };
-}
+const BLOCK_FONT_FILE: &'static str = include_str!("../fonts/block_font.txt");
+const BANNER_FONT_FILE: &'static str = include_str!("../fonts/banner_font.txt");
+const BIG_FONT_FILE: &'static str = include_str!("../fonts/big_font.txt");
 
 pub(crate) fn get_font_data_string(basic_font: &BasicFonts) -> &str {
-    BASIC_FONTS_DATA.get(&basic_font).unwrap()
+    match basic_font {
+        BasicFonts::Big => BIG_FONT_FILE,
+        BasicFonts::Banner => BANNER_FONT_FILE,
+        BasicFonts::Block => BLOCK_FONT_FILE,
+    }
 }
