e25ccc52b85bc15c32fdab8be997a2a58c1a50b7
follow39 <ingang39@gmail.com>
2022-08-16 00:05:33 +0200

    Minor rework printer

--- src/printer.rs
+++ src/printer.rs
@@ -1,11 +1,11 @@
-use crate::art_symbol::ArtSymbol;
-use crate::font::{self, FontError};
+use crate::art_symbol;
+use crate::font;
 use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Debug)]
 pub enum PrinterError {
     Io(std::io::Error),
-    Font(FontError),
+    Font(font::FontError),
 }
 
 impl From<std::io::Error> for PrinterError {
@@ -14,8 +14,8 @@ impl From<std::io::Error> for PrinterError {
     }
 }
 
-impl From<FontError> for PrinterError {
-    fn from(err: FontError) -> PrinterError {
+impl From<font::FontError> for PrinterError {
+    fn from(err: font::FontError) -> PrinterError {
         PrinterError::Font(err)
     }
 }
@@ -38,7 +38,7 @@ impl Printer {
             return Ok(());
         }
         let text_with_font = {
-            let mut text_with_font: Vec<&ArtSymbol> = Vec::with_capacity(text.len());
+            let mut text_with_font: Vec<&art_symbol::ArtSymbol> = Vec::with_capacity(text.len());
             for grapheme in text.graphemes(true) {
                 text_with_font.push(self.font.get(grapheme)?);
             }
@@ -61,26 +61,8 @@ impl Printer {
     }
 
     pub fn render_text(&self, text: &str) -> Result<String, PrinterError> {
-        let text_with_font = {
-            let mut text_with_font: Vec<&ArtSymbol> = Vec::with_capacity(text.len());
-            for grapheme in text.graphemes(true) {
-                text_with_font.push(self.font.get(grapheme)?);
-            }
-            text_with_font
-        };
-        let mut result = String::new();
-        if text_with_font.is_empty() {
-            return Ok(result);
-        }
-        let max_depth: i32 = text_with_font.iter().map(|x| x.depth()).max().unwrap();
-        let max_shift: i32 = text_with_font.iter().map(|x| x.shift()).max().unwrap();
-        for line in -max_shift..(max_depth as i32) {
-            for grapheme in &text_with_font {
-                result += grapheme.get_line(line);
-            }
-            result += "\n";
-        }
-        result += "\n";
-        Ok(result)
+        let mut buf = Vec::<u8>::new();
+        self.print_to(text, &mut buf)?;
+        Ok(String::from_utf8(buf).unwrap())
     }
 }
