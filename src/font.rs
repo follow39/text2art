47c60f34ca9b52e6aed3ff24974da0cb327fefb8
follow39 <ingang39@gmail.com>
2022-08-16 22:30:43 +0200

    Special processing '\n' symbol

--- src/font.rs
+++ src/font.rs
@@ -46,16 +46,27 @@ impl Font {
     pub fn from_string(data: &str) -> Result<Font, FontError> {
         let mut graphemes: HashMap<String, art_symbol::ArtSymbol> = HashMap::new();
         for line in data.lines() {
-            let trim_line = line.trim_end(); // delete whitespaces after data
-            if trim_line.is_empty() || trim_line.as_bytes()[0].eq(&('#' as u8)) {
+            // let trim_line = line.trim_end(); // delete whitespaces after data
+            if line.trim_end().is_empty() || line.as_bytes()[0].eq(&('#' as u8)) {
                 continue;
             }
-            let (symbol, data, shift) = Font::parse_line(trim_line)?;
+            let (symbol, data, shift) = Font::parse_line(line)?;
             graphemes.insert(
-                String::from(symbol),
+                symbol.to_owned(),
                 art_symbol::ArtSymbol::new(symbol, data, shift),
             );
         }
+        let default_space = format!(
+            "{}{}",
+            std::iter::repeat(" ")
+                // .take((graphemes.iter().map(|x| x.1.width()).max().unwrap_or(1).div_ceil(2)) as usize)
+                .take(((graphemes.iter().map(|x| x.1.width()).max().unwrap_or(1) + 1) / 2) as usize)
+                .collect::<String>(),
+            "\\n"
+        );
+        graphemes
+            .entry(" ".to_owned())
+            .or_insert(art_symbol::ArtSymbol::new(" ", default_space.as_ref(), 0));
         Ok(Font { graphemes })
     }
 
@@ -158,8 +169,12 @@ mod tests {
 
     #[test]
     fn test_from_string() {
-        let font_data = "\'a\':0:line_0\\nline_1\\n\n\'b\':0:line_0\\nline_1\\n";
+        let font_data = "\' \':0: \\n\n\'a\':0:line_0\\nline_1\\n\n\'b\':0:line_0\\nline_1\\n";
         let graphemes_expected: HashMap<String, art_symbol::ArtSymbol> = HashMap::from([
+            (
+                String::from(" "),
+                art_symbol::ArtSymbol::new(" ", " \\n", 0),
+            ),
             (
                 String::from("a"),
                 art_symbol::ArtSymbol::new("a", "line_0\\nline_1\\n", 0),
@@ -176,10 +191,14 @@ mod tests {
 
     #[test]
     fn test_get() {
-        let font_data = "\'a\':0:line_0\\nline_1\\n\n\'b\':0:line_0\\nline_1\\n";
+        let font_data = "\' \':0: \\n\n\'a\':0:line_0\\nline_1\\n\n\'b\':0:line_0\\nline_1\\n";
+        let grapheme_space = art_symbol::ArtSymbol::new(" ", " \\n", 0);
         let grapheme_1 = art_symbol::ArtSymbol::new("a", "line_0\\nline_1\\n", 0);
         let grapheme_2 = art_symbol::ArtSymbol::new("b", "line_0\\nline_1\\n", 0);
         let font = font::Font::from_string(font_data).unwrap();
+        let get_result = font.get(" ");
+        assert!(get_result.is_ok());
+        assert_eq!(grapheme_space, *get_result.unwrap());
         let get_result = font.get("a");
         assert!(get_result.is_ok());
         assert_eq!(grapheme_1, *get_result.unwrap());
@@ -192,17 +211,35 @@ mod tests {
 
     #[test]
     fn test_ignore_comments() {
-        let font_data = "#test font data\n\'a\':0:line_0\\nline_1\\n\n\'b\':0:line_0\\nline_1\\n\n#test font data\n";
-        let graphemes_expected: HashMap<String, art_symbol::ArtSymbol> = HashMap::from([
-            (
-                String::from("a"),
-                art_symbol::ArtSymbol::new("a", "line_0\\nline_1\\n", 0),
-            ),
-            (
-                String::from("b"),
-                art_symbol::ArtSymbol::new("b", "line_0\\nline_1\\n", 0),
-            ),
-        ]);
+        let font_data = "#test font data\n\' \':0: \\n\n";
+        let graphemes_expected: HashMap<String, art_symbol::ArtSymbol> = HashMap::from([(
+            String::from(" "),
+            art_symbol::ArtSymbol::new(" ", " \\n", 0),
+        )]);
+        let font_result = font::Font::from_string(font_data);
+        assert!(font_result.is_ok());
+        assert_eq!(graphemes_expected, font_result.unwrap().graphemes);
+    }
+
+    #[test]
+    fn test_default_space() {
+        let font_data = "";
+        let graphemes_expected: HashMap<String, art_symbol::ArtSymbol> = HashMap::from([(
+            String::from(" "),
+            art_symbol::ArtSymbol::new(" ", " \\n", 0),
+        )]);
+        let font_result = font::Font::from_string(font_data);
+        assert!(font_result.is_ok());
+        assert_eq!(graphemes_expected, font_result.unwrap().graphemes);
+    }
+
+    #[test]
+    fn test_custom_space() {
+        let font_data = "\' \':0:custom_space\\n";
+        let graphemes_expected: HashMap<String, art_symbol::ArtSymbol> = HashMap::from([(
+            String::from(" "),
+            art_symbol::ArtSymbol::new(" ", "custom_space\\n", 0),
+        )]);
         let font_result = font::Font::from_string(font_data);
         assert!(font_result.is_ok());
         assert_eq!(graphemes_expected, font_result.unwrap().graphemes);
