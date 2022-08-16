18959fe0ec4427905c75cb88b2fb2208b941448a
follow39 <ingang39@gmail.com>
2022-08-16 10:55:49 +0200

    Implement printer tests

--- src/printer.rs
+++ src/printer.rs
@@ -52,7 +52,6 @@ impl Printer {
             }
             output_stream.write("\n".as_bytes())?;
         }
-        output_stream.write("\n".as_bytes())?;
         Ok(())
     }
 
@@ -64,5 +63,72 @@ impl Printer {
         let mut buf = Vec::<u8>::new();
         self.print_to(text, &mut buf)?;
         Ok(String::from_utf8(buf).unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::basic_fonts;
+    use crate::font;
+    use crate::printer;
+
+    #[test]
+    fn test_print_to_basic() {
+        #[rustfmt::skip]
+        let expected_output = concat!(
+            r" _______             _       _                _   ", "\n",
+            r"|__   __|           | |     | |              | |  ", "\n",
+            r"   | |     ___  ___ | |_    | |_   ___ __  __| |_ ", "\n",
+            r"   | |    / _ \/ __|| __|   | __| / _ \\ \/ /| __|", "\n",
+            r"   | |   |  __/\__ \| |_    | |_ |  __/ )  ( | |_ ", "\n",
+            r"   |_|    \___||___/ \__|    \__| \___|/_/\_\ \__|", "\n",
+        );
+        let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
+            Ok(font) => font,
+            Err(_) => panic!("something wrong with font"),
+        };
+        let mut printer_output_buf = Vec::<u8>::new();
+        let test_text = "Test text";
+
+        let prntr = printer::Printer::with_font(font);
+        prntr.print_to(test_text, &mut printer_output_buf).ok();
+
+        assert_eq!(expected_output.as_bytes(), printer_output_buf);
+    }
+
+    #[test]
+    fn test_print_to_with_default_space() {}
+
+    #[test]
+    fn test_print_to_with_custom_space() {}
+
+    #[test]
+    fn test_print_to_with_default_newline() {}
+
+    #[test]
+    fn test_print_to_with_custom_newline() {}
+
+    #[test]
+    fn test_render() {
+        #[rustfmt::skip]
+        let expected_output = concat!(
+            r" _______             _       _                _   ", "\n",
+            r"|__   __|           | |     | |              | |  ", "\n",
+            r"   | |     ___  ___ | |_    | |_   ___ __  __| |_ ", "\n",
+            r"   | |    / _ \/ __|| __|   | __| / _ \\ \/ /| __|", "\n",
+            r"   | |   |  __/\__ \| |_    | |_ |  __/ )  ( | |_ ", "\n",
+            r"   |_|    \___||___/ \__|    \__| \___|/_/\_\ \__|", "\n",
+        );
+        let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
+            Ok(font) => font,
+            Err(_) => panic!("something wrong with font"),
+        };
+        let test_text = "Test text";
+
+        let prntr = printer::Printer::with_font(font);
+        let redndered_str = prntr.render_text(test_text).ok();
+
+        assert!(redndered_str.is_some());
+        assert_eq!(expected_output, redndered_str.unwrap());
     }
 }
