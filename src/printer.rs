93ec765e6c04d0dbd2f83e4dc47877409d9b1fed
follow39 <ingang39@gmail.com>
2022-08-16 22:38:28 +0200

    Rework tests

--- src/printer.rs
+++ src/printer.rs
@@ -38,19 +38,27 @@ impl Printer {
             return Ok(());
         }
         let text_with_font = {
-            let mut text_with_font: Vec<&art_symbol::ArtSymbol> = Vec::with_capacity(text.len());
-            for grapheme in text.graphemes(true) {
-                text_with_font.push(self.font.get(grapheme)?);
+            let mut text_with_font: Vec<Vec<&art_symbol::ArtSymbol>> =
+                Vec::with_capacity(text.split("\n").count());
+            for text_line in text.split("\n") {
+                let mut text_with_font_line: Vec<&art_symbol::ArtSymbol> =
+                    Vec::with_capacity(text_line.len());
+                for grapheme in text_line.graphemes(true) {
+                    text_with_font_line.push(self.font.get(grapheme)?);
+                }
+                text_with_font.push(text_with_font_line);
             }
             text_with_font
         };
-        let max_depth: i32 = text_with_font.iter().map(|x| x.depth()).max().unwrap();
-        let max_shift: i32 = text_with_font.iter().map(|x| x.shift()).max().unwrap();
-        for line in -max_shift..(max_depth as i32) {
-            for grapheme in &text_with_font {
-                output_stream.write(grapheme.get_line(line).as_bytes())?;
+        for text_with_font_line in text_with_font {
+            let max_depth: i32 = text_with_font_line.iter().map(|x| x.depth()).max().unwrap();
+            let max_shift: i32 = text_with_font_line.iter().map(|x| x.shift()).max().unwrap();
+            for line in -max_shift..(max_depth as i32) {
+                for grapheme in &text_with_font_line {
+                    output_stream.write(grapheme.get_line(line).as_bytes())?;
+                }
+                output_stream.write("\n".as_bytes())?;
             }
-            output_stream.write("\n".as_bytes())?;
         }
         Ok(())
     }
@@ -91,19 +99,19 @@ mod tests {
     fn test_print_to_basic() {
         #[rustfmt::skip]
         let expected_output = concat!(
-            r" _______             _       _                _   ", "\n",
-            r"|__   __|           | |     | |              | |  ", "\n",
-            r"   | |     ___  ___ | |_    | |_   ___ __  __| |_ ", "\n",
-            r"   | |    / _ \/ __|| __|   | __| / _ \\ \/ /| __|", "\n",
-            r"   | |   |  __/\__ \| |_    | |_ |  __/ )  ( | |_ ", "\n",
-            r"   |_|    \___||___/ \__|    \__| \___|/_/\_\ \__|", "\n",
+            r" _______             _   ", "\n",
+            r"|__   __|           | |  ", "\n",
+            r"   | |     ___  ___ | |_ ", "\n",
+            r"   | |    / _ \/ __|| __|", "\n",
+            r"   | |   |  __/\__ \| |_ ", "\n",
+            r"   |_|    \___||___/ \__|", "\n",
         );
         let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
             Ok(font) => font,
             Err(_) => panic!("something wrong with font"),
         };
         let mut printer_output_buf = Vec::<u8>::new();
-        let test_text = "Test text";
+        let test_text = "Test";
         let prntr = printer::Printer::with_font(font);
         prntr.print_to(test_text, &mut printer_output_buf).ok();
 
@@ -111,47 +119,50 @@ mod tests {
     }
 
     #[test]
-    fn test_print_to_with_default_space() {}
-
-    #[test]
-    fn test_print_to_with_custom_space() {}
-
-    #[test]
-    fn test_print_to_with_default_newline() {}
-
-    #[test]
-    fn test_print_to_with_custom_newline() {}
-
-    #[test]
-    fn test_render_empty() {
-        let expected_output = "";
-        let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
-            Ok(font) => font,
-            Err(_) => panic!("something wrong with font"),
-        };
-        let test_text = "";
-        let prntr = printer::Printer::with_font(font);
-        let redndered_str = prntr.render_text(test_text).ok();
-
-        assert!(redndered_str.is_some());
-        assert_eq!(expected_output, redndered_str.unwrap());
-    }
-    #[test]
-    fn test_render_basic() {
+    fn test_print_to_multiline() {
         #[rustfmt::skip]
         let expected_output = concat!(
-            r" _______             _       _                _   ", "\n",
-            r"|__   __|           | |     | |              | |  ", "\n",
-            r"   | |     ___  ___ | |_    | |_   ___ __  __| |_ ", "\n",
-            r"   | |    / _ \/ __|| __|   | __| / _ \\ \/ /| __|", "\n",
-            r"   | |   |  __/\__ \| |_    | |_ |  __/ )  ( | |_ ", "\n",
-            r"   |_|    \___||___/ \__|    \__| \___|/_/\_\ \__|", "\n",
+            r" _______             _   ", "\n",
+            r"|__   __|           | |  ", "\n",
+            r"   | |     ___  ___ | |_ ", "\n",
+            r"   | |    / _ \/ __|| __|", "\n",
+            r"   | |   |  __/\__ \| |_ ", "\n",
+            r"   |_|    \___||___/ \__|", "\n",
+            r" _______             _   ", "\n",
+            r"|__   __|           | |  ", "\n",
+            r"   | |     ___  ___ | |_ ", "\n",
+            r"   | |    / _ \/ __|| __|", "\n",
+            r"   | |   |  __/\__ \| |_ ", "\n",
+            r"   |_|    \___||___/ \__|", "\n",
         );
         let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
             Ok(font) => font,
             Err(_) => panic!("something wrong with font"),
         };
-        let test_text = "Test text";
+        let mut printer_output_buf = Vec::<u8>::new();
+        let test_text = "Test\nTest";
+        let prntr = printer::Printer::with_font(font);
+        prntr.print_to(test_text, &mut printer_output_buf).ok();
+
+        assert_eq!(expected_output.as_bytes(), printer_output_buf);
+    }
+
+    #[test]
+    fn test_render_basic() {
+        #[rustfmt::skip]
+        let expected_output = concat!(
+            r" _______             _   ", "\n",
+            r"|__   __|           | |  ", "\n",
+            r"   | |     ___  ___ | |_ ", "\n",
+            r"   | |    / _ \/ __|| __|", "\n",
+            r"   | |   |  __/\__ \| |_ ", "\n",
+            r"   |_|    \___||___/ \__|", "\n",
+        );
+        let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
+            Ok(font) => font,
+            Err(_) => panic!("something wrong with font"),
+        };
+        let test_text = "Test";
         let prntr = printer::Printer::with_font(font);
         let redndered_str = prntr.render_text(test_text).ok();
 
