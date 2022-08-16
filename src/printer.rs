c0d8a1316b1f5364e49c7e650830a1a3e093caf2
follow39 <ingang39@gmail.com>
2022-08-16 23:05:35 +0200

    Add fill method

--- src/printer.rs
+++ src/printer.rs
@@ -22,11 +22,20 @@ impl From<font::FontError> for PrinterError {
 
 pub struct Printer {
     font: font::Font,
+    fill_grapheme: Option<String>,
 }
 
 impl Printer {
     pub fn with_font(font: font::Font) -> Printer {
-        Printer { font }
+        Printer {
+            font,
+            fill_grapheme: None,
+        }
+    }
+
+    pub fn set_fill_grapheme(mut self, grapheme: Option<String>) -> Self {
+        self.fill_grapheme = grapheme;
+        self
     }
 
     pub fn print_to(
@@ -40,7 +49,7 @@ impl Printer {
         let text_with_font = {
             let mut text_with_font: Vec<Vec<&art_symbol::ArtSymbol>> =
                 Vec::with_capacity(text.split("\n").count());
-            for text_line in text.split("\n") {
+            for text_line in text.split("\n").filter(|&x| !x.is_empty()) {
                 let mut text_with_font_line: Vec<&art_symbol::ArtSymbol> =
                     Vec::with_capacity(text_line.len());
                 for grapheme in text_line.graphemes(true) {
@@ -55,7 +64,10 @@ impl Printer {
             let max_shift: i32 = text_with_font_line.iter().map(|x| x.shift()).max().unwrap();
             for line in -max_shift..(max_depth as i32) {
                 for grapheme in &text_with_font_line {
-                    output_stream.write(grapheme.get_line(line).as_bytes())?;
+                    match &self.fill_grapheme {
+                        Some(x) => output_stream.write(grapheme.get_line(line).replace(" ", x).as_bytes())?,
+                        None => output_stream.write(grapheme.get_line(line).as_bytes())?,
+                    };
                 }
                 output_stream.write("\n".as_bytes())?;
             }
@@ -168,5 +180,28 @@ mod tests {
 
         assert!(redndered_str.is_some());
         assert_eq!(expected_output, redndered_str.unwrap());
+    }
+
+    #[test]
+    fn test_set_fill_grapheme() {
+        #[rustfmt::skip]
+        let expected_output = concat!(
+            r"._______............._...", "\n",
+            r"|__...__|...........|.|..", "\n",
+            r"...|.|.....___..___.|.|_.", "\n",
+            r"...|.|..../._.\/.__||.__|", "\n",
+            r"...|.|...|..__/\__.\|.|_.", "\n",
+            r"...|_|....\___||___/.\__|", "\n",
+        );
+        let font = match font::Font::from_basic(basic_fonts::BasicFonts::Big) {
+            Ok(font) => font,
+            Err(_) => panic!("something wrong with font"),
+        };
+        let mut printer_output_buf = Vec::<u8>::new();
+        let test_text = "Test";
+        let prntr = printer::Printer::with_font(font).set_fill_grapheme(Some(".".to_owned()));
+        prntr.print_to(test_text, &mut printer_output_buf).ok();
+
+        assert_eq!(expected_output.as_bytes(), printer_output_buf);
     }
 }
